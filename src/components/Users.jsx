55a000fe59a8a525b557624eecb7c451a0f1e0c3
Mahmudul107 <mahmudulislam378@gmail.com>
2023-05-09 16:20:49 +0600

    update user to database

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -3,15 +3,13 @@ import React, { useState } from "react";
 import { Link, useLoaderData } from "react-router-dom";
 
 const Users = () => {
-  const loadedUsers = useLoaderData();
-  const [users, setUsers] = useState(loadedUsers)
 
+    const loadedUsers = useLoaderData();
+    const [users, setUsers] = useState(loadedUsers);
 
-  const handleDeleteUser = (_id) => {
-    // Perform the logic to delete the user with the given _id
-    console.log(`Deleting user with _id: ${_id}`);
-    // Make an API request or update the user state accordingly
-    fetch(`http://localhost:5000/users/${_id}`, {
+    const handleDelete = _id =>{
+        console.log( 'delete', _id);
+        fetch(`http://localhost:5000/users/${_id}`, {
             method: 'DELETE'
         })
         .then(res=> res.json())
@@ -19,30 +17,30 @@ const Users = () => {
             console.log(data);
             if(data.deletedCount>0){
                 alert('deleted successfully');
-                const remainingUsers = users.filter(user => user._id !== _id)
-                setUsers(remainingUsers)
+                const remaining = users.filter(user => user._id !== _id);
+                setUsers(remaining); 
             }
         })
-  };
+    }
 
-  return (
-    <div>
+    return (
+        <div>
             <h2>{users.length}</h2>
             <div>
                 {
                     users.map(user => <p 
                         key={user._id}
                         > {user.name} : {user.email} {user._id}
+                        <Link to={`/update/${user._id}`}>
+                            <button>Update</button>
+                        </Link>
                          <button
-                            onClick={ () => handleDeleteUser(user._id)}
+                            onClick={ () => handleDelete(user._id)}
                         >X</button> </p>)
                 }
             </div>
-            <Link to='/'><button>Back To Home</button></Link>
         </div>
-  );
-};
+    );
+};  
 
 export default Users;
-
-
--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-vars */
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
@@ -8,23 +7,27 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import Users from './components/Users.jsx';
+import Update from './components/Update.jsx';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App/>
+    element: <App></App>,
   },
   {
-    path: "/users",
-    element: <Users/>,
+    path:'/users',
+    element: <Users></Users>,
     loader: () => fetch('http://localhost:5000/users')
+  },
+  {
+    path: '/update/:id',
+    element: <Update></Update>, 
+    loader: ({params}) => fetch(`http://localhost:5000/users/${params.id}`)
   }
 ]);
 
-
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-     <RouterProvider router={router} />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
